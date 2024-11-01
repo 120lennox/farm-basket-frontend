c1f773e4a3bdfa6ade99176a5d518ed7147aacd6
Lennox Mountain <lennoxmountain67@gmail.com>
2024-11-01 23:07:13 +0200

    initial configurations

--- farmbasket/app/page.tsx
+++ farmbasket/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div>
-      <div>
-        
+      <div className="flex h-screen justify-center items-center">
+        <h1>farm basket</h1>
       </div>
     </div>
   );
