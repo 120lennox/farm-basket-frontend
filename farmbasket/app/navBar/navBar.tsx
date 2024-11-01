0909f715a03645a1f61110ed7cc4f43d127fac7a
osmanDaud <167176444+osmanDaud@users.noreply.github.com>
2024-11-01 23:58:07 +0200

    added some componants to the header

--- farmbasket/app/navBar/navBar.tsx
+++ farmbasket/app/navBar/navBar.tsx
@@ -1,7 +1,20 @@
 export default function NavBar() {
   return (
-    <div>
-      <h1>this will be our NavBar</h1>
+    <div className=" text-white">
+      <div className=" flex justify-between items-center h-24 max-w-[1240px] mx-4 px-4">
+        <h1 className=" w-full text-3xl font-bold">
+          FARM <span className=" text-[#00df9a]"> BUSKET</span>
+        </h1>
+        <ul className=" flex bg-slate-400 rounded-lg">
+          <li className=" p-2"> Collections</li>
+          <li className=" p-2"> Explore</li>
+          <li className=" p-2"> Shops</li>
+        </ul>
+        <div className=" flex p-4">
+          <h1 className=" p-2 ">Login</h1>
+          <h1 className=" p-2">Register</h1>
+        </div>
+      </div>
     </div>
   );
 }
--- farmbasket/app/page.tsx
+++ farmbasket/app/page.tsx
@@ -1,8 +1,10 @@
+import NavBar from "./navBar/navBar";
+
 export default function Home() {
   return (
     <div>
       <div>
-        
+        <NavBar />
       </div>
     </div>
   );
