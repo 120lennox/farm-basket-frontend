996564f184392b8ced89ea2d9ba55fd7548ce43c
Lennox Mountain <lennoxmountain67@gmail.com>
2024-11-01 23:21:24 +0200

    added inter font

--- farmbasket/app/page.tsx
+++ farmbasket/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <div className="flex h-screen justify-center items-center">
-        <h1>farm basket</h1>
+        <h1 className="font-inter" >farm basket</h1>
       </div>
     </div>
   );
