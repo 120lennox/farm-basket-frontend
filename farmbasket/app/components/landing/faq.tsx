c7a10b569d33c65bcd524a288b18e4e413d0255a
Lennox Mountain <lennoxmountain67@gmail.com>
2024-11-02 22:16:22 +0200

    added questions on faq

--- farmbasket/app/components/landing/faq.tsx
+++ farmbasket/app/components/landing/faq.tsx
@@ -1,7 +1,48 @@
 export default function Faq() {
   return (
     <div>
-      hello faq
+      <div>
+        <div className="font-bold text-[48px]">
+          Frequently Asked Questions
+        </div>
+        <div className="mt-5">
+          <div>
+            <hr className="mb-5 h-0.5 bg-gradient-to-r from-gray-300 to-gray-100 border-0" />
+          </div>
+          <div>
+            <div className="mt-10 mb-10 text-[40px]">
+                How do I create an account on the platform?  
+            </div>
+            <div>
+              <hr className="mb-5 h-0.5 bg-gradient-to-r from-gray-300 to-gray-100 border-0" />
+            </div>
+          </div>
+          <div>
+            <div className="mt-10 mb-10 text-[40px]">
+              How do I list my products?  
+            </div>
+            <div>
+              <hr className="mb-5 h-0.5 bg-gradient-to-r from-gray-300 to-gray-100 border-0" />
+            </div>
+          </div>
+          <div>
+            <div className="mt-10 mb-10 text-[40px]">
+              What payment methods are accepted?  
+            </div>
+            <div>
+              <hr className="mb-5 h-0.5 bg-gradient-to-r from-gray-300 to-gray-100 border-0" />
+            </div>
+          </div>
+          <div>
+            <div className="mt-10 mb-10 text-[40px]">
+              Is my personal information safe?  
+            </div>
+            <div>
+              <hr className="mb-5 h-0.5 bg-gradient-to-r from-gray-300 to-gray-100 border-0" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
