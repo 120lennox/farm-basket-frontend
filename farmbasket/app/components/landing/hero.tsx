eab5411211ae22a53b43cbac5f251cb9cd844979
Lennox Mountain <lennoxmountain67@gmail.com>
2024-11-02 20:13:29 +0200

    starting to work on hero

--- farmbasket/app/components/landing/hero.tsx
+++ farmbasket/app/components/landing/hero.tsx
@@ -1,7 +1,7 @@
-export default function Hero() {
-  return (
-    <div>
+export default function hero() {
+    return (
+        <div>
       <h1>thi is hero page</h1>
-    </div>
+        </div>
   );
 }
