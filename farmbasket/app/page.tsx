9bc966075c689fb8cf33999b18f65a3cdafd2128
osmanDaud <167176444+osmanDaud@users.noreply.github.com>
2024-11-03 00:40:19 +0200

    changed the way componets are being dispalyed

--- farmbasket/app/page.tsx
+++ farmbasket/app/page.tsx
@@ -7,21 +7,16 @@ import NavBar from "./navBar/navBar";
 export default function Home() {
   return (
     <div className="mx-8">
-      <div className="w-full bg-black h-16 mt-0">
-        <h1>Nav bar here</h1>
-      </div>
+      <div className="w-full bg-black h-16 mt-0"></div>
       <div>
         <NavBar />
-        <h1 className=" w-full h-[400px] bg-green-950 text-center font-bold">
-          content that mountain is working on
-        </h1>
-        <Footer />
       </div>
       <section className="mt-16 mb-10">
         <div>
           <Faq />
         </div>
       </section>
+      <Footer />
     </div>
   );
 }
