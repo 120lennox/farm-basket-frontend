fec22117fa6a58bd1c69705812a75c763f74b026
osmanDaud <167176444+osmanDaud@users.noreply.github.com>
2024-11-02 22:32:11 +0200

    added so styling the header compoent

--- farmbasket/app/navBar/navBar.tsx
+++ farmbasket/app/navBar/navBar.tsx
@@ -1,18 +1,20 @@
 export default function NavBar() {
   return (
-    <div className=" text-white">
-      <div className=" flex justify-between items-center h-24 max-w-[1240px] mx-4 px-4">
-        <h1 className=" w-full text-3xl font-bold">
-          FARM <span className=" text-[#00df9a]"> BUSKET</span>
-        </h1>
-        <ul className=" flex bg-slate-400 rounded-lg">
-          <li className=" p-2"> Collections</li>
-          <li className=" p-2"> Explore</li>
-          <li className=" p-2"> Shops</li>
+    <div className="text-white uppercase border-b">
+      <div className="flex justify-between items-center mx-auto h-24 max-w-[1240px] px-4">
+        <div className="flex items-center">
+          <h1 className="text-3xl font-extrabold ">
+            FARM <span className="text-[#00df9a]">BASKET</span>
+          </h1>
+        </div>
+        <ul className="flex space-x-4 bg-slate-400 p-[10px] font-bold rounded-[45px] items-center">
+          <li className="p-2">Shops</li>
+          <li className="p-2">Explore</li>
+          <li className="p-2">Collections</li>
         </ul>
-        <div className=" flex p-4">
-          <h1 className=" p-2 ">Login</h1>
-          <h1 className=" p-2">Register</h1>
+        <div className="flex space-x-4 p-4 font-bold">
+          <h1 className="p-2">Login</h1>
+          <h1 className="p-2">Register</h1>
         </div>
       </div>
     </div>
