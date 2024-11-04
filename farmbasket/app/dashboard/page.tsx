5bf0dcb6462886f0d753b94d71f83224f4aa77f3
unknown <BSC-COM-32-22@unima.ac.mw>
2024-11-04 15:13:40 +0200

    statistics and stock

--- farmbasket/app/dashboard/page.tsx
+++ farmbasket/app/dashboard/page.tsx
@@ -6,14 +6,24 @@ export default function dashboard(){
 
         <h1 className="text-2xl font-bold mb-6">shop name</h1>
          <div className="space-y-4">
-          <button className="flex items-center space-x-2 h-60 text-lg text-gray-700 hover:text-black">
+          <button className="flex items-center space-x-2 h-16 text-lg text-gray-700 hover:text-black">
             <span>Notifications</span>
             <BellIcon className='w-6 h-6'/>
           </button>
 
-          <button className="flex items-center h-1 space-x-2 text-lg text-gray-700 hover:text-black">
+          <button className="flex items-center h-16 space-x-2 text-lg text-gray-700 hover:text-black">
             <span>Messages</span>
             <ChatBubbleBottomCenterTextIcon className="w-6 h-6" />
+          </button>
+
+          <button className="flex items-center h-16 space-x-2 text-lg text-gray-700 hover:text-black">
+            <span>Statistics</span>
+            <ChartBarIcon className="w-6 h-6" />
+          </button>
+
+           <button className="flex items-center space-x-2 text-lg text-white bg-green-500 rounded-full px-4 py-2 mt-4 hover:bg-green-600">
+            <span>Stock</span>
+            <BuildingStorefrontIcon className="w-6 h-6" />       
           </button>
           </div>
         </aside>
