4c8608ef49286285d267c078e8fc5e6dc50b7810
Lennox Mountain <lennoxmountain67@gmail.com>
2024-11-10 22:49:42 +0200

    day 2. next logic. fixing unknown bugs

--- farmbasket/app/components/products/shop_card.tsx
+++ farmbasket/app/components/products/shop_card.tsx
@@ -1,18 +1,22 @@
 import { fetchShopCardData } from "@/app/lib/data";
 
 const ShopCard = async()=>{
-    const shop = await fetchShopCardData();
-    console.log(shop)
+    const shops = await fetchShopCardData();
+    console.log(shops)
     return <div>
-        <div>
-            <div>
-                <div></div>
-                <div>
+        <div className="flex justify-between items-center">
+            {shops?.map((shop)=>(
+                <div key={shop.id} className="bg-green-50 rounded-3xl flex flex-row ">
                     <div></div>
-                    <div></div>
-                    <div></div>
+                    <div>
+                        <div>
+                            {shop.name}
+                        </div>
+                        <div></div>
+                        <div></div>
+                    </div>
                 </div>
-            </div>
+            ))}
         </div>
     </div>
 }
