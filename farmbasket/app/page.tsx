221d166e9a444dc1f2dd62c50f61c6acad17dc0a
osmanDaud <167176444+osmanDaud@users.noreply.github.com>
2024-11-12 01:30:42 +0200

    removed all imports from main page.tsx

--- farmbasket/app/page.tsx
+++ farmbasket/app/page.tsx
@@ -1,14 +1,3 @@
-import Footer from "./components/landing/footer";
-import NavBar from "./components/landing/navbar";
-import Login from "./components/login/login";
-import SingUp from "./components/signUp/signUp";
-
 export default function Home() {
-  return (
-    <div>
-      <NavBar />
-      <SingUp />
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 }
