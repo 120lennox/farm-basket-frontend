7096c8b4dcf90da4cfe38f77078dd666e10bd77c
Lisbethkumakanga <BSC-COM-32-22@unima.ac.mw>
2024-11-11 22:46:22 +0200

    messages

--- farmbasket/app/dashboard/components/sidebar.tsx
+++ farmbasket/app/dashboard/components/sidebar.tsx
@@ -10,10 +10,10 @@ export default function Sidebar(){
                     <div className="flex items-center justify-center space-x-2 h-36 text-lg text-white">
                         <Link href="/notification">Notifications</Link>
                     </div>
-                    {/*<div className="flex items-center justify-center h-5 space-x-2 text-lg text-white">
+                    <div className="flex items-center justify-center h-5 space-x-2 text-lg text-white">
                        <Link href="/messages">Messages</Link> 
                     </div>
-                    <div className="flex items-center justify-center h-32 space-x-2 text-lg text-white">
+                    {/*<div className="flex items-center justify-center h-32 space-x-2 text-lg text-white">
                         <Link href="/statistics">Statistics</Link>
                     </div> 
                     <div className="flex items-center justify-center h-10 space-x-2 text-lg text-white ">
