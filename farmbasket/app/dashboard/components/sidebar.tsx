27cefb098b7b037583fde67563b9e8d6a6193d47
Lisbethkumakanga <BSC-COM-32-22@unima.ac.mw>
2024-11-11 22:44:22 +0200

    sidebar on notification

--- farmbasket/app/dashboard/components/sidebar.tsx
+++ farmbasket/app/dashboard/components/sidebar.tsx
@@ -6,11 +6,11 @@ export default function Sidebar(){
                 <div className="flex items-center justify-center  text-2xl font-bold mb-6 ">
                     Shop Name
                 </div>
-                {/* <div> 
+                <div> 
                     <div className="flex items-center justify-center space-x-2 h-36 text-lg text-white">
                         <Link href="/notification">Notifications</Link>
                     </div>
-                    <div className="flex items-center justify-center h-5 space-x-2 text-lg text-white">
+                    {/*<div className="flex items-center justify-center h-5 space-x-2 text-lg text-white">
                        <Link href="/messages">Messages</Link> 
                     </div>
                     <div className="flex items-center justify-center h-32 space-x-2 text-lg text-white">
@@ -18,8 +18,8 @@ export default function Sidebar(){
                     </div> 
                     <div className="flex items-center justify-center h-10 space-x-2 text-lg text-white ">
                     <Link href ="/stock">Stock</Link>
-                </div>
-            </div> */}
+                </div>*/}
+            </div> 
             </div>
     </div>
 }
