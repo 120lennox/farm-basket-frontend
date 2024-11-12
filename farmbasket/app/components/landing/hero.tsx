55c7516d49b1bacb041b23beaf33d211cc4e669b
osmanDaud <167176444+osmanDaud@users.noreply.github.com>
2024-11-12 17:29:11 +0200

    trying to resolve conflicts

--- farmbasket/app/components/landing/hero.tsx
+++ farmbasket/app/components/landing/hero.tsx
@@ -30,30 +30,30 @@ export default function Hero() {
                 What's Trending?
               </div>
             </div>
-            <div className="flex justify-between items-center mt-14">
-              <div className="bg-gray-400 h-80 w-72 rounded-md" >
-
-              </div>
-              <div className="bg-gray-400 h-80 w-72 rounded-md" >
-                
-              </div>
-              <div className="bg-gray-400 h-80 w-72 rounded-md" >
-                
-              </div>
-            </div>
-            <div className="mt-10 flex justify-center items-center flex-row space-x-2">
-              <div className="bg-gray-400 rounded-full h-4 w-4" >
-
-              </div>
-              <div className="bg-gray-400 rounded-full h-4 w-4" >
-
-              </div>
-              <div className="bg-gray-400 rounded-full h-4 w-4" >
-
-              </div>
-            </div>
+          </div>
+          <div className="mt-8">
+            <button className="bg-gray-400 p-4 px-10 rounded-2xl font-normal">
+              Get Started
+            </button>
           </div>
         </div>
->>>>>>> 66453e700d7fe7d2a9bed160a3807b2d69317f32
+<<<<<<<<< Temporary merge branch 1
+        <div className="mt-10 mb-5">
+          <div className="text-[48px] font-semibold">What's Trending?</div>
+        </div>
+        <div className="flex justify-between items-center mt-14">
+          <div className="bg-gray-400 h-80 w-72 rounded-md"></div>
+          <div className="bg-gray-400 h-80 w-72 rounded-md"></div>
+          <div className="bg-gray-400 h-80 w-72 rounded-md"></div>
+        </div>
+        <div className="mt-10 flex justify-center items-center flex-row space-x-2">
+          <div className="bg-gray-400 rounded-full h-4 w-4"></div>
+          <div className="bg-gray-400 rounded-full h-4 w-4"></div>
+          <div className="bg-gray-400 rounded-full h-4 w-4"></div>
+        </div>
+      </div>
+    </div>
+=========
+>>>>>>>>> Temporary merge branch 2
   );
 }
