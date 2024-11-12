0628e9d77c6bb98a23d7f2a4851d6a0381447ac9
Lisbethkumakanga <BSC-COM-32-22@unima.ac.mw>
2024-11-12 16:19:15 +0200

    slight changes

--- farmbasket/app/dashboard/page.tsx
+++ farmbasket/app/dashboard/page.tsx
@@ -5,20 +5,20 @@ export default function dashboard(){
     return <div>
             
               <div className="flex ">
-                  <div className="bg-white h-screen w-3/12">
+                  <div className="bg-white h-screen w-1/6">
                     <div className="mx-4 mt-5">
                      <Sidebar />
                     </div>
                   </div>
                    <div className="flex-1 relative">
                         <Header/>
-                        <div  className="absolute top-[64px] left-0 right-0 border-t border-black">
+      
                           
                         </div>
-                      
-                      </div> 
                       </div>
-                  </div>
+                      </div> 
+                      
+                  
           
     
     
