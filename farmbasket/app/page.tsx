96fa19b1fcf3829bde8a79a53b59f2a076492994
osmanDaud <167176444+osmanDaud@users.noreply.github.com>
2024-11-13 08:40:11 +0200

    trying so resolve conficts

--- farmbasket/app/page.tsx
+++ farmbasket/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Faq from "./components/landing/faq";
 import Hero from "./components/landing/hero";
 
@@ -18,48 +17,9 @@ export default function Home() {
         </section>
       </div>
       <Footer />
-=======
-const products=()=>{
-  return(
-    <div>
-       <div className="text-col bg-slate-600 text-start content-center">
-    <div >
-       <b><h1>Mlimi Adyerere Agri store</h1></b> 
->>>>>>> de8a3e0c4f0c7e622bed38e4af2315824d66964c
     </div>
-    <div >
-        <h4>Dziko ndi wanu, ndalama wathu</h4>
-    </div>
-    <div>
-        <h4>40% off discount</h4>
-    </div>
-</div>
+   
 
-<div><div className="flex row space-x-6 space-y-6">
-  
-  <div className="flex-container box-border h-80 w-80 p-4 bg-slate-300 border-4 .." >
-  </div>
-  <div className="flex-container box-border h-80 w-80 p-4 bg-slate-300 border-4 .." >
-  </div>
-  <div className="flex-container box-border h-80 w-80 p-4 bg-slate-300 border-4 " >
-  </div> 
-  <div className="flex-container box-border h-80 w-80 p-4 bg-slate-300 border-4 .." >
-  </div>
-</div>
-<div className="flex row space-x-6 space-y-6">
-  <div className="flex-container box-border h-80 w-80 p-4 bg-slate-300 border-4 .." >
-  </div>
-  <div className="flex-container box-border h-80 w-80 p-4 bg-slate-300 border-4 .." >
-  </div>
-  <div className="flex-container box-border h-80 w-80 p-4 bg-slate-300 border-4 .." >
-  </div>
-  <div className="flex-container box-border h-80 w-80 p-4 bg-slate-300 border-4 " >
-  </div> 
-</div>
-
-</div>
-</div>
-    
+ 
   );
 }
-export default products;
