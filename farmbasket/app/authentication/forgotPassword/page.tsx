db275ca0a9aa6b3070db0cda7f0824a4373cb916
osmanDaud <167176444+osmanDaud@users.noreply.github.com>
2024-11-20 15:55:23 +0200

    making a forgot password modal

--- farmbasket/app/authentication/forgotPassword/page.tsx
+++ farmbasket/app/authentication/forgotPassword/page.tsx
@@ -2,7 +2,7 @@ export default function ForgotPassword() {
   return (
     <div>
       <div className=" fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center ">
-        <div className=" bg-white h-[400px] w-[350px] flex just">
+        <div className=" bg-white h-[400px] w-[350px] flex flex-col justify-center items-center">
           <h1>forgot password</h1>
           <h2>Enter your Email</h2>
         </div>
