6872de160590bd081c2d666b63273ba613dba927
osmanDaud <167176444+osmanDaud@users.noreply.github.com>
2024-11-20 15:31:16 +0200

    addded a forget password feature

---
+++ farmbasket/app/authentication/forgotPassword/page.tsx
@@ -0,0 +1,12 @@
+export default function ForgotPassword() {
+  return (
+    <div>
+      <div className=" fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center ">
+        <div className=" bg-white h-[400px] w-[350px] flex just">
+          <h1>forgot password</h1>
+          <h2>Enter your Email</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
