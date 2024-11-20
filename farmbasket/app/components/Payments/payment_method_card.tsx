b9202bd218259ac726eacb0e8efb7adad97d0e94
bed-com-28-22 <160385884+bed-com-28-22@users.noreply.github.com>
2024-11-21 01:43:04 +0200

    adding branch

---
+++ farmbasket/app/components/Payments/payment_method_card.tsx
@@ -0,0 +1,53 @@
+const paymentmethod=()=>{
+    return(
+        <div>
+            <div className="items-center justify-between p-5">
+                <div className="min-h-screen space-y-10 items-end m-10 p-10 justify-center rounded-2xl ms-20 mt-20 w-1/3 bg-gray-200 container">
+                    <div className="flex  justify-center   items-center rounded-2xl">Account/payment methods</div>
+                    <div className=" flex iterm-center justify-center text-green-600 font-fira rounded-2xl "><b>Choose Your Payment Method</b></div>
+                    <div className="flex flex-row ccontainer mx-auto px-4 rounded-2xl bg-white border-green-600  py-8 space-x-10">
+                        <div className=" flex flex-row">
+                         ZZe
+                        </div>
+                        <div className="">
+                        MMMMMMMMMMM
+                        </div>
+                        <div className="">
+                            WWWW
+                        </div>
+                    </div>
+                    <div className=" flex flex-row ccontainer mx-auto px-4 rounded-2xl bg-white border-green-600  space-x-10 py-8">
+                        <div className="">
+                            YYy
+                        </div>
+                        <div className="">
+                            XXX XXX XX XXX XXX
+                        </div>
+                        <div className="">
+                            BBBBB
+                        </div>
+                    </div>
+                    <div className=" flex flex-row container mx-auto px-4  border-green-600 bg-white rounded-2xl space-x-10 py-8">
+                    <div className="">
+                            ZZZ
+                    </div>
+                    <div className="">
+                          XXXX  BBB TT
+                    </div>
+                    <div className="">
+                            CCCCC
+                    </div>   
+                    </div>
+                    <div className="flex items-center justify-center h-10 w-12/12 text-white w-90 bg-green-500 rounded-2xl">
+                       <button><b>Submit</b></button>     
+                    </div>
+                    <div className="flex  items-center justify-center">
+                        Powered by Pay Changu
+                    </div>
+                </div>
+            </div>   
+        </div>
+    );
+}
+
+export default paymentmethod;
