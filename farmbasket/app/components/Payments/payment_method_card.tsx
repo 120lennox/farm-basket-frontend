e501cad9db8c35bf8e7c9090d28e61794ce4d343
bed-com-28-22 <160385884+bed-com-28-22@users.noreply.github.com>
2024-11-21 18:07:48 +0200

    adding a feature

--- farmbasket/app/components/Payments/payment_method_card.tsx
+++ farmbasket/app/components/Payments/payment_method_card.tsx
@@ -6,15 +6,28 @@ const paymentmethod=()=>{
                     <div className="flex  justify-center   items-center rounded-2xl">Account/payment methods</div>
                     <div className=" flex iterm-center justify-center text-green-600 font-fira rounded-2xl "><b>Choose Your Payment Method</b></div>
                     <div className="flex flex-row ccontainer mx-auto px-4 rounded-2xl bg-white border-green-600  py-8 space-x-10">
-                        <div className=" flex flex-row">
-                         ZZe
-                        </div>
-                        <div className="">
-                        MMMMMMMMMMM
-                        </div>
-                        <div className="">
-                            WWWW
-                        </div>
+                    <div className="flex items-center justify-between border-2 border-green-500 rounded-lg p-4">
+        <div className="flex items-center space-x-4">
+          <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Airtel_logo.svg/512px-Airtel_logo.svg.png" // Replace with your Airtel logo URL
+                 alt="Airtel"
+                className="h-8"
+               />
+               <p className="text-lg font-bold">XXX XXXX XX XXX</p>
+       </div>
+              <div className="h-6 w-6 flex items-center justify-center rounded-full border-2 border-green-500 text-green-500">
+                <svg
+                 xmlns="http://www.w3.org/2000/svg"
+                 fill="none"
+                 viewBox="0 0 24 24"
+                 strokeWidth={2}
+                   stroke="currentColor"
+                  className="h-4 w-4"
+                   >
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+               </svg>
+             </div>
+                   </div>
                     </div>
                     <div className=" flex flex-row ccontainer mx-auto px-4 rounded-2xl bg-white border-green-600  space-x-10 py-8">
                         <div className="">
