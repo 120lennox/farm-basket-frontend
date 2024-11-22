c20ebc0c7d7925205b95c8adbd18e362044d1a7e
osmanDaud <167176444+osmanDaud@users.noreply.github.com>
2024-11-22 21:12:01 +0200

    changed the routing link

--- farmbasket/app/authentication/login/page.tsx
+++ farmbasket/app/authentication/login/page.tsx
@@ -90,7 +90,7 @@ export default function Login() {
               </p>
               <div className="mt-3">
                 <Link
-                  href="/authentification/signUp"
+                  href="/authentication/signUp"
                   className="bg-green-900 text-white rounded-full px-4 py-2 hover:bg-green-700 focus:outline-none"
                 >
                   SIGN UP
