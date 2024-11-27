9e0248a5db7a403094ae68eb03b9e1995bf02f91
osmanDaud <167176444+osmanDaud@users.noreply.github.com>
2024-11-27 07:52:35 +0200

    deleted a payment file

--- farmbasket/app/payment/page.tsx
+++
@@ -1,9 +0,0 @@
-import PaymentMethod from "../components/Payments/payment_method_card"
-
-export default function paymentCard(){
-    return <div>
-        <div>
-            <PaymentMethod/>
-        </div>
-    </div>
-}
