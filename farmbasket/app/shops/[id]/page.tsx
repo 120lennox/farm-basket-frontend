ab6e4a2d9deb3a7dd0a9ebbc036ed4e111973456
128kcmt <bsc-com-04-22@unima.ac.mw>
2024-11-27 09:05:27 +0200

    maping a shop products

--- farmbasket/app/shops/[id]/page.tsx
+++ farmbasket/app/shops/[id]/page.tsx
@@ -16,76 +16,59 @@ const shopStore = async({params}: PageProps)=>{
     const {id} = await params
     const shopid = await fetchShopid(id);
     const shopProduct = await fetchShopProducts(id)
+    console.log(shopProduct)
     return <div>
         <div className="mx-8">
-                <div key={shopid.shopid}>
-                    <div className="mt-20 flex flex-col ">
-                        <div className="bg-yellow-50 w-full rounded-xl shadow-md p-8 flex flex-col gap-y-2">
-                            <div className="text-[36px] font-extrabold text-green-700">{shopid.name}</div>
-                            <div className="text-[24px] font-semibold">
-                                {shopid.description}
-                            </div>
-                            <div className="text-[20px] font-extralight">
-                                40% off discount
-                            </div>
-                        </div>
+            {shopProduct.map((shop)=> (
 
-                        <div>
-                            <div></div>
-                            <div></div>
-                            <div></div>
-                        </div>
+<div key={shopid.shopid}>
+<div className="mt-20 flex flex-col ">
+    <div className="bg-yellow-50 w-full rounded-xl shadow-md p-8 flex flex-col gap-y-2">
+        <div className="text-[36px] font-extrabold text-green-700">{shop.shop.name}</div>
+        <div className="text-[24px] font-semibold">
+            {shop.shop.description}
+        </div>
+        <div className="text-[20px] font-extralight">
+            40% off discount
+        </div>
+    </div>
 
-                        <div className="mt-10">
-                            <h1 className="text-[36px] font-medium">Products for you</h1>
-                        </div>
+    <div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
 
-                        <div className="bg-gradient-to-r from-green-500 to-green-100">
-                            <hr  />
-                        </div>
+    <div className="mt-10">
+        <h1 className="text-[36px] font-medium">Products for you</h1>
+    </div>
 
-                        <div className="mt-10 mb-10">
-                            <div>
-                                <div className="rounded-sm">
-                                    <Image className="rounded-2xl"
-                                           src={shopProduct.image}
-                                           alt="Fertilizer bag"
-                                           width={400}
-                                           height={500} 
-                                    />
+    <div className="bg-gradient-to-r from-green-500 to-green-100">
+        <hr  />
+    </div>
+
+    <button className='h-86 hover:shadow-xl hover:shadow-black bg-white '>
+                            <div className='flex flex-col relative w-64 h-80 '>
+                                <img src={shop.image} alt={shop.name} className='-mt-1 w-64 flex h-64'/>
+                                <div className='flex flex-col mt-2'>
+                                    <p className='text-sm first-letter:uppercase lowercase bg-gray-200 w-full'>{shop.name}</p>
                                 </div>
-                                <div className="absolute -bottom-[55%] left-[3rem]">
-
-                                    <div className="bg-neutral-50 rounded-3xl p-5 flex flex-col space-y-1">
-                                        <div className="flex flex-row justify-between">
-                                            <div className="text-[18px] font-semibold">
-                                                {shopProduct.name}
-                                            </div>
-                                            <div className="text-[18px] font-semibold">
-                                                <span>MK</span>{shopProduct.price}
-                                            </div>
-                                        </div>
-                                        <div className="w-1/2">
-                                            {shopProduct.description}
-                                        </div>
-                                        <div></div>
-                                        <div>
-                                            <div className="">
-                                                <Link className="bg-green-500 text-white rounded-full py-2 px-4 font-semibold" href="/">add to cart</Link>
-                                            </div>
-                                            <div></div>
-                                        </div>
-                                    </div>
+                                <div className='flex '>
+                                    <p>{shop.price}</p>
+                                </div>
+                                <div  className='flex gap-x-1'>
+                                    <p></p>
                                 </div>
                             </div>
-                        </div>
+                        </button>
 
-                        <div className="mt-10">
+    <div className="mt-10">
 
-                        </div>
-                    </div>
+    </div>
+</div>
 
-                </div>
+</div>
+            ))}
         </div>
     </div>
 }
