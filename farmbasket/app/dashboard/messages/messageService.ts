d6b062893d699312b92a566c150a89d2f9e8b409
Lennox Mountain <lennoxmountain67@gmail.com>
2024-11-30 11:49:35 +0200

    messaging services

--- farmbasket/app/dashboard/messages/messageService.ts
+++ farmbasket/app/dashboard/messages/messageService.ts
@@ -12,11 +12,38 @@ const axiosInstance = axios.create({
     },
 })
 
-// handling interceptors
+/**
+ * authenticating axios with token, so it realizes the owner of the messages
+ */
+
+axiosInstance.interceptors.request.use(
+    (config)=>{
+        const token = localStorage.getItem('authToken')
+
+        if (token){
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    },
+    (error) => Promise.reject(error)
+)
+
 
 //handle message routes 
 export const messageService = {
     getInbox: async () =>{
-        const response = await axios.create()
-    }
+        try{
+            const response = await axiosInstance.get('https://farm-basket3.onrender.com/messages/inbox')
+            return response.data
+        } catch(error){
+            if (axios.isAxiosError(error)){
+                // other logic here'
+                console.log('error', error)
+                throw error.response?.data || new Error('failed to fetch')
+            }
+            throw error
+        }
+    },
+
+    // other services
 }
